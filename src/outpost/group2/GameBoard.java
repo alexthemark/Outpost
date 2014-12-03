d73cf3bb0a8a850b52e83ded10c72f0c943f377c
ys2816@columbia.edu <yunsun.seu@gmail.com>
2014-12-02 19:04:35 -0500

    new version of offensive

--- src/outpost/group2/GameBoard.java
+++ src/outpost/group2/GameBoard.java
@@ -66,12 +66,15 @@ public class GameBoard {
 					if (!cell.hasWater && cell.owner != id) {
 						currentCell.landValue++;
 					}
+					if (cell.hasWater && cell.owner != id) {
+						currentCell.waterValue++;
+					}
 				}
 				currentCell.landValue = (int) (landMultiplier * 100 * ((double) currentCell.landValue/(double) (4*influenceDistance*influenceDistance)));
 				for (int k = 0; k < BOARD_SIZE; k++) {
 					for (int l = 0; l < BOARD_SIZE; l++) {
 						comparisonCell = grid[k][l];
-						if (comparisonCell.owner != id && validCellForAnalysis(comparisonCell) && comparisonCell.hasWater) {
+						if (comparisonCell.owner != id && validCellForAnalysis(comparisonCell) && comparisonCell.hasWater && waterReachable(comparisonCell, influenceDistance)) {
 							currentCell.waterValue += waterMultiplier * (1/ (double)manhattanDist(comparisonCell, currentCell));
 						}
 					}
@@ -100,16 +103,20 @@ public class GameBoard {
 	}
 	
 	// Returns a number between 0 and 100, where 100 is the best defensive score, and 0 is the worst.
-	public int calculateDefensiveScore(Outpost movingPost, Pair testPos, int playerId, Pair homespace) {
+	public int calculateDefensiveScore(Outpost movingPost, Pair testPos, int playerId, Pair homespace, int influence) {
 		ArrayList<Outpost> playerOutposts = outposts.get(playerId);
 		int MAX_DIST = 200;
 		int combinedDistances = 0;
 		for (Outpost testPost : playerOutposts) {
 			// we don't want to count the post we're looking to move
 			if (testPost.id == movingPost.id)
-				break;
+				continue;
 			int dist = manhattanDist(testPos, testPost);
+			if (dist > influence/2)
 				combinedDistances += (MAX_DIST - dist);
+			// Penalty for being too close
+			if (dist < influence/2)
+				combinedDistances -= (influence - dist) * (influence - dist) * (influence - dist) * (influence - dist);
 		}
 		combinedDistances += MAX_DIST - manhattanDist(homespace, testPos);
 		return (int) (combinedDistances/(playerOutposts.size()))/2;
@@ -122,61 +129,84 @@ public class GameBoard {
 	public double calculateOffensiveScore(Outpost movingPost, Pair testPos, int playerId, int influenceDist,ArrayList<Pair> intruderList) {
 		//return 5*grid[testPos.x][testPos.y].landValue;
 		double dis = influenceDist * 2;
-		double inf_range = 3*2*influenceDist;
+		//double inf_range = 3*2*influenceDist;
+		double inf_range = 3*influenceDist;
 		double value = 0;
-		ArrayList<GridCell> influencedCells = getInfluencedCells(testPos, influenceDist);
+		double max_value = 0;
+	/*	ArrayList<GridCell> influencedCells = getInfluencedCells(testPos, influenceDist);
 		for (GridCell cell : influencedCells) {
 			if (cell.owner == GridCell.NO_OWNER) {
 				value++;
 			}
-		}
+		}*/
 		//System.out.println("size of intruder: " + intruderList.size());
 		for(int i = 0; i < intruderList.size(); i++)
 		{
+			value = 0;
+	//		System.out.println("dis: " + distance(testPos, intruderList.get(i)));
+			
 			if (distance(intruderList.get(i), testPos) < inf_range) {
+				//System.out.println("dis in range: " + distance(testPos, intruderList.get(i)));
+
+				//System.out.println("testPos-x: " + testPos.x + ", y: " + testPos.y);
 				//System.out.println("intruder x: " + intruderList.get(i).x + ", y: " + intruderList.get(i).y);
 
-				if (grid[intruderList.get(i).x][ intruderList.get(i).y].owner == GridCell.NO_OWNER) {
+				//if (grid[intruderList.get(i).x][ intruderList.get(i).y].owner == GridCell.NO_OWNER) {
+				if (grid[testPos.x][testPos.y].owner == GridCell.NO_OWNER) {
+
 					//System.out.println("cell x: " + testPos.x + ", y: " + testPos.y + ", non_owner");
-					value += 5 + inf_range - distance(testPos, intruderList.get(i));
-					return value;
+					value = 60 - distance(testPos, intruderList.get(i));
+					//System.out.println("way 60");
+					//return value;
 						//System.out.println("neutral res becomes mine!");
 					} 
-						else
-						if (grid[intruderList.get(i).x][ intruderList.get(i).y].owner != playerId) {
+						//else if (grid[intruderList.get(i).x][ intruderList.get(i).y].owner != playerId) {
+				else if (grid[testPos.x][ testPos.y].owner != playerId) {
+					//System.out.println("way 120");
 							//System.out.println("not owned by our ourself");
-							if(playerId==0 && (intruderList.get(i).x < dis || intruderList.get(i).y < dis ))
+							if(playerId == 0 && (intruderList.get(i).x < dis || intruderList.get(i).y < dis ))
 							{
-								value += 35 + inf_range - distance(testPos, intruderList.get(i));
-								return value;
+								value = 120 - distance(testPos, intruderList.get(i));
+								//return value;
 							}
-							if(playerId==1 && (intruderList.get(i).x > BOARD_SIZE-dis || intruderList.get(i).y < dis ))
+							else if(playerId == 1 && (intruderList.get(i).x > BOARD_SIZE-dis || intruderList.get(i).y < dis ))
 									{
 								//System.out.println(" booundary");
 
-								value += 35 + inf_range - distance(testPos, intruderList.get(i));
-								return value;
+								value = 120 - distance(testPos, intruderList.get(i));
+								//return value;
 									}
-							if(playerId==3 && (intruderList.get(i).x < dis || intruderList.get(i).y > BOARD_SIZE-dis ))
+							else if(playerId == 3 && (intruderList.get(i).x < dis || intruderList.get(i).y > BOARD_SIZE-dis ))
 									{
-								value += 35 + inf_range - distance(testPos, intruderList.get(i));
-								return value;
+								value = 120 - distance(testPos, intruderList.get(i));
+								//return value;
 									}
-							if(playerId==2 && (intruderList.get(i).x > BOARD_SIZE -dis || intruderList.get(i).y > BOARD_SIZE-dis ))
+							else if(playerId == 2 && (intruderList.get(i).x > BOARD_SIZE -dis || intruderList.get(i).y > BOARD_SIZE-dis ))
 									{
-										value += 35 + inf_range - distance(testPos, intruderList.get(i));
-										return value;
+										value = 120 - distance(testPos, intruderList.get(i));
+										//return value;
 									}
-							//System.out.println("not booundary");
+							//System.out.println("not boundary");
 
-							value += 25 + inf_range - distance(testPos, intruderList.get(i));
-							return value;
+							else 
+								{value = 100 - distance(testPos, intruderList.get(i));
+								//System.out.println("way 100");
+								}
+							//System.out.println("!!!!value: " + value);
+							if(value > max_value)
+								{
+								//System.out.println("large than max value!!!!!!!");
+								max_value = value;
+								}
+								
+							//return value;
 					}
 				}
 			}
 		
-		return value;
+		return max_value;
 	}
+
 	
 	public Resource getResources(int id) {
 		Resource resourcesToReturn = new Resource(0,0);
@@ -209,6 +239,15 @@ public class GameBoard {
 			}
 		}
 		return influencedCells;
+	}
+	
+	private boolean waterReachable(GridCell waterCell, int influenceDist) {
+		ArrayList<GridCell> influencingCells = getInfluencedCells(waterCell, influenceDist);
+		for (GridCell cell : influencingCells) {
+			if (!cell.hasWater)
+				return true;
+		}
+		return false;
 	}
 	
 	public int manhattanDist(Pair pr1, Pair pr2) {
--- src/outpost/group2/Player.java
+++ src/outpost/group2/Player.java
@@ -20,7 +20,7 @@ public class Player extends outpost.sim.Player {
 	public void init() {
 		homespaces.put(0, new Pair(0,0));
 		homespaces.put(1, new Pair(BOARD_SIZE - 1,0));
-		homespaces.put(2, new Pair(BOARD_SIZE -1, BOARD_SIZE -1));
+		homespaces.put(2, new Pair(BOARD_SIZE - 1, BOARD_SIZE -1));
 		homespaces.put(3, new Pair(0, BOARD_SIZE -1));
 		homespace = homespaces.get(id);
 		currentTick = 0;
@@ -34,6 +34,7 @@ public class Player extends outpost.sim.Player {
 	public ArrayList<movePair> move(ArrayList<ArrayList<Pair>> outpostPairs, Point[] gridin, int influenceDist, int L, int W, int T){
     	long startTime = System.currentTimeMillis();
 		currentTick++;
+		boolean endGame = T - currentTick < 200;
 		ArrayList<movePair> ourNextMoves = new ArrayList<movePair>();   
 		ArrayList<Pair> moveSpaces = new ArrayList<Pair>();
 		ArrayList<ArrayList<Outpost>> outposts = new ArrayList<ArrayList<Outpost>>();
@@ -53,18 +54,22 @@ public class Player extends outpost.sim.Player {
 	    	board.updateSupplyLines(homespaces);
 	    	//printOutposts(outposts, id);
 	    	Resource ourResources = board.getResources(this.id); 
-	    //	System.out.printf("[GROUP2][LOG] We have %d water, %d land at start\n", ourResources.water, ourResources.land);
+	    	System.out.printf("[GROUP2][LOG] We have %d water, %d land at start\n", ourResources.water, ourResources.land);
 	    	int waterMultiplier = calculateResourceMultiplier(outposts.get(id).size(), W, ourResources.water);
-	    //	System.out.println("[GROUP2][LOG] Water multiplier: " + waterMultiplier);
+	    	System.out.println("[GROUP2][LOG] Water multiplier: " + waterMultiplier);
 	    	int landMultiplier = calculateResourceMultiplier(outposts.get(id).size(), L, ourResources.land);
-	    //	System.out.println("[GROUP2][LOG] Land multiplier: " + landMultiplier);
+	    	if (endGame) {
+	    		landMultiplier += 1;
+	    		landMultiplier *= 5;
+	    	}
+	    	System.out.println("[GROUP2][LOG] Land multiplier: " + landMultiplier);
 	    	ArrayList<Pair> pairsToCheck = new ArrayList<Pair>();
 	    	for (Pair outpost : outpostPairs.get(id)) {
 	    		pairsToCheck.addAll(surroundingPairs(outpost, board));
 	    	}
 	    	board.calculateResourceValues(pairsToCheck, this.id, influenceDist, waterMultiplier, landMultiplier);
     	} catch (OwnersNotUpdatedException e) {
-    	//	System.err.println("[GROUP2][ERROR] Must update the owners before calling this function");
+    		System.err.println("[GROUP2][ERROR] Must update the owners before calling this function");
     	}
  
     	// Great, now we know the resource value of each cell. Let's now look at each outpost, and see
@@ -72,10 +77,11 @@ public class Player extends outpost.sim.Player {
     	for (int outpostId = 0; outpostId < outposts.get(id).size(); outpostId++) {
     		Outpost outpost = outposts.get(id).get(outpostId);
     		double bestScore = 0;
-    		Pair bestPair = new Pair();
+    		Pair bestPair = new Pair(outpost);
+    		moveSpaces.add(outpost);
     		ArrayList<Pair> surroundingPairs = surroundingPairs(outpost, board);
     		for (Pair testPos : surroundingPairs) {
-    			int defensiveVal = board.calculateDefensiveScore(outpost, testPos, id, homespace);
+    			int defensiveVal = board.calculateDefensiveScore(outpost, testPos, id, homespace, influenceDist);
     			double waterResourceVal = board.getWaterResourceVal(testPos);
     			double landResourceVal = board.getLandResourceVal(testPos);
     			// add surround opponents' outposts
@@ -84,9 +90,10 @@ public class Player extends outpost.sim.Player {
     			ArrayList<Pair> intruderList = cal.findIntruder(myConvexHull, outpostPairs, this.id);
     			
     			int offensiveVal = (int) /*((double) currentTick/ (double) T) **/ board.calculateOffensiveScore(outpost, testPos, id, influenceDist,intruderList);
-    			double currentScore = waterResourceVal + landResourceVal + defensiveVal +  5*offensiveVal;
-    			//System.out.printf("Point %d, %d water val: %f, land val: %f, defensive val: %d, offensive val: %d\n", testPos.x, testPos.y, waterResourceVal, landResourceVal, defensiveVal,5*offensiveVal);
-    			if (currentScore > bestScore && !moveSpaces.contains(bestPair)) {
+    			//int offensiveVal = 5*(int) board.calculateOffensiveScore(outpost, testPos, id, influenceDist,intruderList);
+    			double currentScore = defensiveVal + waterResourceVal + landResourceVal + offensiveVal;
+    			//System.out.printf("Point %d, %d water val: %f, land val: %f, defensive val: %d, offensive val: %d\n", testPos.x, testPos.y, waterResourceVal, landResourceVal, defensiveVal,offensiveVal);
+    			if (currentScore >= bestScore && !moveSpaces.contains(bestPair)) {
     				bestScore = currentScore;
     				bestPair = testPos;
     			}
@@ -96,7 +103,7 @@ public class Player extends outpost.sim.Player {
     		ourNextMoves.add(thisMove);
     	}
     	long endTime = System.currentTimeMillis();
-    	//System.out.println("[GROUP2][LOG] Took " + (endTime - startTime) + "milliseconds");
+    	System.out.println("[GROUP2][LOG] Took " + (endTime - startTime) + "milliseconds");
     	return ourNextMoves;
 	}
 	
